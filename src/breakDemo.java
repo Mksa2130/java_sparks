1b6e70bf29f3180eede4e4e9037c2c1e21f9d988
Mksa2130 <mksa2130@gmail.com>
2025-10-09 10:12:52 +0530

    main

--- src/breakDemo.java
+++ src/breakDemo.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class breakDemo {
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            if (i==5)//false true
+            {
+                break;
+
+            }
+            System.out.println("Namaste india "+i);
+        }
+    }
 }
--- src/contDemo.java
+++ src/contDemo.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class contDemo {
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+    }
 }
