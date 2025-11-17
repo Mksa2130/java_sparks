f4275717ce9c24685952261a152b6da6864b7e54
Mksa2130 <mksa2130@gmail.com>
2025-11-17 15:08:43 +0530

    main

--- src/variable/instance.java
+++ src/variable/instance.java
@@ -1,4 +1,17 @@
 package variable;
 
 public class instance {
+
+    //instance variable
+    String name="Nazia";
+    int age=23;           // premitive variable
+public static void main(String[] args)
+
+{
+instance i=new instance(); //non premitive variable ->refrence variable this is also called object
+    System.out.println("name "+ i.name);
+    System.out.println("Age is "+ i.age);
+
+}
+
 }
